dd77b43f3ac6ddd57728a2d1d6afeeae86d27376
rachelharu <rachel.haru9@gmail.com>
2022-08-30 15:47:26 -0700

    create function to fetch data from api

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: { 
+            apikey: 'ed7394f',
+            s: 'avengers' 
+        }
+    } );
+
+    console.log(response.data);
+};
+
+fetchData();
+
+
+// http://www.omdbapi.com/?apikey=[ed7394f]&
