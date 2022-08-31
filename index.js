d4bac37446863a1f3983a431e660b56a0f022f1a
rachelharu <rachel.haru9@gmail.com>
2022-08-31 12:10:42 -0700

    set up js function to only call search function after 500ms

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//function to search api database for movie title
 const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: { 
@@ -10,6 +11,17 @@ const fetchData = async (searchTerm) => {
 };
 
 const input = document.querySelector('input');
-input.addEventListener('input', (event) => {
-    fetchData(event.target.value);
-});
+
+//sets time interval to 1 second. when the user stops typing fetchData request will be sent to api.
+let timeoutId;
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+}, 500);
+};
+
+input.addEventListener('input', onInput);
+
