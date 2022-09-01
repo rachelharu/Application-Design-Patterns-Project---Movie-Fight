49f24ec8a22fdf8ddc8efcb5272fda0f5c41d07c
rachelharu <rachel.haru9@gmail.com>
2022-08-31 21:19:42 -0700

    create dropdown menu for search input

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const fetchData = async (searchTerm) => {
         }
     } );
 
+    if (response.data.Error) {
+        return [];
+    }
+
     return response.data.Search;
 };
 
@@ -15,7 +19,7 @@ const input = document.querySelector('input');
 //sets time interval. when the user stops typing fetchData request will be sent to api.
 const onInput = async event => {
    const movies = await fetchData(event.target.value);
-   
+   //creates div element to render image of movie poster
    for (let movie of movies) {
     const div = document.createElement('div');
     
